41e001be9621c02ca7bebace1fe6c11eefe91f2e
GuanYeFan <1072970912@qq.com>
2019-06-05 10:19:41 +0800

    Initial commit

---
+++ src/main/resources/com/qf/springboothello/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.qf.springboothello.mapper;
+
+import com.qf.springboothello.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
